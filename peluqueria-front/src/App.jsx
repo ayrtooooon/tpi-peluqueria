f6f28407188b532cd17062d96ab9def32c926079
NicoBoscia <nicoboscia04@gmail.com>
2025-06-05 12:12:12 -0300

    BarbersView

--- peluqueria-front/src/App.jsx
+++ peluqueria-front/src/App.jsx
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import FormPage from "./pages/FormPage";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import BarberView from "./pages/BarbersView";
 import Register from "./pages/Register";
 import Footer from "./components/Footer";
 import { ToastContainer } from "react-toastify";
@@ -12,17 +13,22 @@ import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
+    <div className="d-flex flex-column min-vh-100">
     <BrowserRouter>
       <NavBar />
+      <main className="flex-grow-1">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/turnos" element={<FormPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/barbersView" element={<BarberView />} />
       </Routes>
+      </main>
       <ToastContainer />
       <Footer />
     </BrowserRouter>
+    </div>
   );
 }
 
