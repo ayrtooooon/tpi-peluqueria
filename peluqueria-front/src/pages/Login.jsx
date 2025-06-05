0871cfb9cd5058927ecd5cc8ed983877ccfc77b4
NicoBoscia <nicoboscia04@gmail.com>
2025-06-05 11:05:30 -0300

    Update Login.jsx

--- peluqueria-front/src/pages/Login.jsx
+++ peluqueria-front/src/pages/Login.jsx
@@ -44,7 +44,7 @@ const Login = () => {
     const password = passwordRef.current.value;
 
     try {
-      const res = await fetch("http://localhost:3000/login", {
+      const res = await fetch("http://localhost:3000/users", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
